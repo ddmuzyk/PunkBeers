40b5691b695440c38dd568ff219f0f6bc498846e
ddmuzyk <domino2k1@gmail.com>
2023-06-29 00:05:43 +0200

    fixed page btn styling

--- src/routes/beer/beer.jsx
+++ src/routes/beer/beer.jsx
@@ -7,15 +7,18 @@ const Beer = () => {
 
   const {name, tagline, id, ingredients, description, abv, ibu, image_url} = useLoaderData();
 
-  // Getting the page associated with the beer based on the fact that every page has 9 beers.
+  // Getting the page associated with the beer based on the fact that every page has 9 beers
   const page = id % 9 > 0 ? Math.floor(id / 9) + 1 : Math.floor(id / 9);
+
  
 
   return (
     <div className="beer-container">
       <Header/>
       <div className="back-btn-container">
-        <Link to={page === 1 ? '/' : `/page/${page}`}>Back to page</Link>
+        <Link to={page === 1 ? '/' : `/page/${page}`}>
+          <button className="back-to-page-btn">Back to page</button>
+        </Link>
       </div>
       <div className="beer-img-container">
         <img src={image_url} alt="Beer Image" className="beer-img"/>
