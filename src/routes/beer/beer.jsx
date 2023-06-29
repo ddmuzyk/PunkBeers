2397fe55c1ffadf46d77ea19db36572acf098701
ddmuzyk <domino2k1@gmail.com>
2023-06-29 02:33:20 +0200

    tying different ways of displayin ingredients data

--- src/routes/beer/beer.jsx
+++ src/routes/beer/beer.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router-dom";
 import './Beer.css';
 import { Header } from "../../components/header/Header";
 import { Link } from "react-router-dom";
+import { Ingredient } from "../../components/ingredients/Ingredient";
 
 const Beer = () => {
 
@@ -25,29 +26,14 @@ const Beer = () => {
       </div>
         <h1 className="beer-name">{name}</h1>
         <h2>{tagline}</h2>
-        <p>{description}</p>
+        <p className="beer-description">{description}</p>
         <h3>Abv: {abv}</h3>
         <h3>Ibu: {ibu}</h3>
         <h3>Ingredients</h3>
         <div className="ingredients">
           {Object.keys(ingredients).map((ingredient) => {
             const subIngredients = ingredients[ingredient];
-            return (
-              <div className={`${ingredient} ingredient`} key={ingredient}>
-                <h4 className="ingredient-title">{`${ingredient.slice(0,1).toUpperCase()}${ingredient.slice(1)}`}</h4>
-                {Array.isArray(subIngredients) ? (
-                  subIngredients.map((subIngredient, i) => {
-                    return (
-                      <div key={i}>
-                        <h5>{subIngredient.name}</h5>
-                      </div>
-                    )
-                  })
-                )
-                :
-                <h5>{subIngredients}</h5>}
-              </div>
-            )
+            return <Ingredient key={ingredient} ingredients={ingredients} ingredient={ingredient}/>
           })}
           {/* {ingredients.hops.map((hop) => {
             
